2ea163bcfd39cf6b7f7a8aa23aac8b34e04cae83
Edwin ngila kyalo <edwinngilakyalo@gmail.com>
2023-12-05 12:07:51 +0300

    final commit

--- src/page/PublishScreen.js
+++ src/page/PublishScreen.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap"
 const PublishScreen=()=>{
     return(
         <Container fluid>
-             <div className="row justify-content-center align-items-center">
+             <div style={{height:"100vh"}} className="row justify-content-center align-items-center">
                  <Button className="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4" style={{backgroundColor:"#092332",color:"white"}}>PUBLISH</Button>
              </div>
         </Container>
